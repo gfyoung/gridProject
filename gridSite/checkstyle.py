532f712b39faf178d90fd74b70349e36707e1fb9
gfyoung <gfyoung@mit.edu>
2015-12-24 23:59:59 -0800

    Convert print statements to functions

--- gridSite/checkstyle.py
+++ gridSite/checkstyle.py
@@ -4,6 +4,8 @@ Note that this file is self-reflective, so checkstyle will
 also be run on the checkstyle.py file itself!
 """
 
+from __future__ import print_function
+
 from fabric.api import hide, local
 from os import getcwd
 from sys import exit
@@ -11,8 +13,8 @@ from sys import exit
 try:
     import pep8
 except ImportError:
-    print "\nFAILURE: Cannot run Python checkstyle! Missing library pep8"
-    print "Please run '(sudo) pip install pep8' to install\n"
+    print("\nFAILURE: Cannot run Python checkstyle! Missing library pep8")
+    print("Please run '(sudo) pip install pep8' to install\n")
     exit(-1)
 
 errorsFound = False
@@ -22,7 +24,7 @@ excludedPy = ['.svn', 'CVS', '.bzr', '.hg', '.git',
               '__pycache__', '.tox', 'tmpFile*']
 pythonErrorFile = "pythonStyleErrors.txt"
 
-print "\nPerforming Python checkstyle..."
+print("\nPerforming Python checkstyle...")
 
 try:
     with hide('aborts', 'running'):
@@ -30,16 +32,16 @@ try:
               .format(projDir=currentDir,
                       excludedPy=','.join(excludedPy),
                       errFile=pythonErrorFile))
-        print "SUCCESS: Python checkstyle passed!"
+        print("SUCCESS: Python checkstyle passed!")
 except:
     errorsFound = True
-    print "FAILURE: Python checkstyle errors were found!\n" \
-          "Please look at {projDir}/styleErrors.txt for " \
-          "more information".format(projDir=currentDir)
+    print("FAILURE: Python checkstyle errors were found!\n"
+          "Please look at {projDir}\styleErrors.txt for "
+          "more information".format(projDir=currentDir))
 
-print "\nOverall result of checkstyle is:"
+print("\nOverall result of checkstyle is:")
 
 if errorsFound:
-    print "FAILURE: Checkstyle complete, but errors were found!\n"
+    print("FAILURE: Checkstyle complete, but errors were found!\n")
 else:
-    print "SUCCESS: Checkstyle complete, and no errors were found!\n"
+    print("SUCCESS: Checkstyle complete, and no errors were found!\n")
