bf84e528e738f8048046d2bd4aa2c83f0f63305b
gfyoung <gfyoung@mit.edu>
2015-12-25 23:10:18 -0800

    Added more settings and urls to gridSite

--- gridSite/gridSite/settings.py
+++ gridSite/gridSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'is4+7tga=oi6!wetdatx_i4od%hi28()fzqj)8^o_w#yyth4lb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
--- gridSite/gridSite/urls.py
+++ gridSite/gridSite/urls.py
@@ -1,9 +1,10 @@
-from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/gridApp/', permanent=False)),
     url(r'^gridApp/', include('gridApp.urls', namespace='gridApp')),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(r'^static/(?P<path>.*)$', document_root='trollApp/static')
